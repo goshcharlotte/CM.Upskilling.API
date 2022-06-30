1326eba9c20c4f7637f8db7886c09ea290d6583c
Charlotte Murray <cmurray@and.digital>
2022-06-30 11:01:11 +0100

    Initial Commit

---
+++ NBS.Practice.API.UnitTest/Controllers/HealthControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using NBS.Practice.API.Controllers;
+
+namespace NBS.Practice.API.UnitTest;
+
+public class Tests
+{
+    private HealthController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        _controller = new HealthController();
+    }
+
+    [Test]
+    public void Ping_ReturnsTrue()
+    {
+        // Arrange
+
+        // Act
+        var response = _controller.Ping();
+
+        // Assert
+        Assert.That(response == true);
+    }
+
+    [Test]
+    public void Echo_ReturnsInput()
+    {
+        // Arrange
+        var testString = "this is a test string";
+
+        // Act
+        var response = _controller.Echo(testString);
+
+        // Assert
+        Assert.That(response == testString);
+    }
+}
---
+++ NBS.Practice.API/Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NBS.Practice.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        public HealthController()
+        {
+        }
+
+        [HttpGet]
+        [Route("ping")]
+        public bool Ping()
+        {
+            return true;
+            //return Ok(true);
+        }
+
+        [HttpGet]
+        [Route("echo")]
+        public string Echo(string str)
+        {
+            return str;
+            //return Ok(str);
+        }
+    }
+}
+
